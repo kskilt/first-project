66f0e9c5488292a9147d2e5ce844818d99eb4b5f
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-09 11:10:06 -0400

    added player count to Squad list

--- First-Project-Scraping/lib/Squad-Lists.rb
+++ First-Project-Scraping/lib/Squad-Lists.rb
@@ -1,13 +1,14 @@
 class Squad-Lists
-  attr_reader :rank, :name, :score, :margin_of_victory, :squad
+  attr_reader :rank, :name, :score, :margin_of_victory, :squad, :player_count
   @@all = []
 
-  def initialize(:rank, :name, :score, :margin_of_victory, :squad)
+  def initialize(:rank, :name, :score, :margin_of_victory, :squad, :player_count)
     @rank = rank
     @name = name
     @score = score
     @margin_of_victory = margin_of_victory
     @squad = squad
+    @player_count = player_count
     @@all << self
   end
 
