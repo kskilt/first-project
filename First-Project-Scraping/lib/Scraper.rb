0307cc93b53f7cc58310efd7c1b164028bc1e4a3
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-09 11:08:11 -0400

    Added Squad-Lists.rb which is the second layer

--- First-Project-Scraping/lib/Scraper.rb
+++ First-Project-Scraping/lib/Scraper.rb
@@ -1,9 +1,10 @@
+require "open-uri"
 require 'pry'
+
 class Scraper
   url = "https://listfortress.com/"
 
   def self.scrape_index_page(url)
     index_page = Nokogiri::HTML(open(url))
-    binding.pry
   end
 end
---
+++ First-Project-Scraping/lib/Squad-Lists.rb
@@ -0,0 +1,3 @@
+class Squad-Lists
+
+end
