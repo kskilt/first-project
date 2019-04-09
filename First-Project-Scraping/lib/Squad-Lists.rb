2e99e75dbc0967f6c6338d30cc24b64aaf28a2ae
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-09 11:08:46 -0400

    Added Initialize/Attr Accessor to Squad-list

--- First-Project-Scraping/lib/Squad-Lists.rb
+++ First-Project-Scraping/lib/Squad-Lists.rb
@@ -1,3 +1,17 @@
 class Squad-Lists
+  attr_reader :rank, :name, :score, :margin_of_victory, :squad
+  @@all = []
 
+  def initialize(:rank, :name, :score, :margin_of_victory, :squad)
+    @rank = rank
+    @name = name
+    @score = score
+    @margin_of_victory = margin_of_victory
+    @squad = squad
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
